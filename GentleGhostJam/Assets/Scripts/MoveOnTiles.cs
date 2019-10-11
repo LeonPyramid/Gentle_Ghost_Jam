c2ba28c952f3ec7fa6ca35ad7bf1c48d37114524
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-10-11 05:42:15 +0200

    Mouvements Perso et PNJ stable

---
+++ GentleGhostJam/Assets/Scripts/MoveOnTiles.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum orient {n,s,e,w};
+public class MoveOnTiles : MonoBehaviour
+{
+    public orient curOrient = orient.n;
+    public float speed;
+    private bool isMoving; //Savoir si le perso est en mouvement
+    public bool GetIsMoving()
+    {
+        return isMoving;
+    }
+
+    public Vector2 tilePos;
+    private float distFromTile;
+    private float horizontal;
+    private float vertical;
+    public Vector2 direct;
+    private Rigidbody2D rgbd;
+    public float horTemp;
+    public float verTemp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilePos = new Vector2(this.transform.position.x,this.transform.position.y); // place la tuile de base
+        isMoving = false;
+        direct = new Vector2(0,0);
+        rgbd = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical   = Input.GetAxis("Vertical");
+        if(!isMoving){
+            if(Mathf.Abs(horizontal)>Mathf.Abs(vertical)){
+                isMoving = true;
+                horTemp = horizontal;
+                verTemp = 0;
+                if(horizontal>0){
+                    curOrient = orient.e;
+                    direct = new Vector2(1,0);
+                }
+                else{
+                    curOrient = orient.w;
+                    direct = new Vector2(-1,0);
+                }
+            }
+            else if (Mathf.Abs(horizontal)<Mathf.Abs(vertical)){
+                isMoving = true;
+                verTemp = vertical;
+                horTemp = 0;
+                if(vertical>0){
+                    curOrient = orient.n;
+                    direct = new Vector2(0,1);
+                }
+                else{
+                    curOrient = orient.s;
+                    direct = new Vector2(0,-1);
+                }
+            }
+        }
+        else{
+            /*if (horTemp*horizontal <0){
+                direct = -direct;
+                horTemp = horizontal;
+                if (horizontal>0){
+                    tilePos = new Vector2(tilePos.x-32,tilePos.y);
+                }
+                else
+                {
+                    tilePos = new Vector2(tilePos.x+32,tilePos.y);
+                }
+            }
+            if (verTemp*vertical<0){
+                direct = -direct;
+                verTemp = vertical;
+                if (vertical>0){
+                    tilePos = new Vector2(tilePos.x,tilePos.y-32);
+                }
+                else
+                {
+                    tilePos = new Vector2(tilePos.x,tilePos.y+32);
+                }
+            }   Code de demi-tour qui fait planter les collisions, à utiliser si tu prévient les colisions avec les murs*/
+            distFromTile = CalculateDistFrom(tilePos);
+            if (distFromTile < 32){
+                Move (speed,direct);
+            }
+            else {
+                tilePos = transform.position;
+                if (horizontal*horTemp<=0&&vertical*verTemp<=0){
+                    isMoving = false;
+                    direct = new Vector2(0,0);
+                }
+                else{
+                    Move (speed,direct);
+                }
+            }
+        }
+
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        isMoving = false;
+        direct = new Vector2(0,0);
+        this.transform.position = tilePos;
+        verTemp = 0;
+        horTemp = 0;
+    }
+
+
+
+    float CalculateDistFrom (Vector2 coord){
+        float x = Mathf.Abs(coord.x - transform.position.x);
+        float y = Mathf.Abs(coord.y - transform.position.y);
+        return Mathf.Sqrt(x*x + y*y);
+        
+    }
+    void Move (float speed,Vector2 direction){
+        direction = direction*speed;
+        rgbd.MovePosition(new Vector2(transform.position.x+direction.x,
+                                        transform.position.y+direction.y));
+    }
+
+    
+
+}
---
+++ GentleGhostJam/Assets/Scripts/MovePNJ.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum moveType {one,loop,reverse};
+
+public class MovePNJ : MonoBehaviour
+{
+    public moveType mouvement;
+    public orient curOrient = orient.n;
+
+    public float speed;
+    private int i;
+    private float temps;
+    private bool isMoving; //Savoir si le perso est en mouvement
+    public bool GetIsMoving()
+    {
+        return isMoving;
+    }
+    private Vector2 tilePos;
+
+    private float waitTime;
+    private float distFromTile;
+    private Vector2 direct;
+    private Rigidbody2D rgbd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        tilePos = new Vector2(this.transform.position.x,this.transform.position.y); // place la tuile de base
+        isMoving = false;
+        direct = new Vector2(0,0);
+        rgbd = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        temps -= Time.deltaTime; // fait attendre le PNJ
+        if (temps <=0){
+            if(!isMoving){
+                tilePos = GetComponent<ListeNoeuds>().liste[i].transform.position; //Définit la direction en fonction du noeud suivant
+                direct = CalculateDirectOf(tilePos);
+                if (direct != new Vector2(0,0)){
+                    isMoving = true;
+                    SetOrient (direct);
+                }
+            }
+            else{
+
+                distFromTile = CalculateDistFrom(tilePos);
+                if (distFromTile>0){
+                    Move (speed,direct);
+                }
+                else {
+                    tilePos = transform.position;
+                    isMoving = false;
+                    direct = new Vector2(0,0);
+                    Move (speed,direct);
+                    if (i != GetComponent<ListeNoeuds>().liste.GetLength(0)-1){
+                        temps = GetComponent<ListeNoeuds>().liste[i].temps;     //récupère le temps d'attente
+                            i++;
+                    }
+                    else if (mouvement == moveType.one){
+                        Destroy(GetComponent<MovePNJ>());
+                    }
+                }
+            }
+        }
+        
+    }
+
+    float CalculateDistFrom (Vector2 coord){
+        float x = Mathf.Abs(coord.x - transform.position.x);
+        float y = Mathf.Abs(coord.y - transform.position.y);
+        return Mathf.Sqrt(x*x + y*y);
+        
+    }
+    void Move (float speed,Vector2 direction){
+        direction = direction*speed;
+        rgbd.MovePosition(new Vector2(transform.position.x+direction.x,
+                                        transform.position.y+direction.y));
+    }
+
+    Vector2 CalculateDirectOf(Vector2 coord){
+        float lx = coord.x - transform.position.x;
+        if (lx != 0 ){
+            lx = lx /Mathf.Abs(lx);
+        }
+        float ly = coord.y - transform.position.y;
+        if (ly != 0){
+            ly = ly /Mathf.Abs(ly);
+        }
+        return new Vector2(lx,ly);
+        
+    }
+
+    orient SetOrient (Vector2 dir){
+        if(dir.x != 0){
+            if(dir.x>0){
+                return orient.e;
+            }
+            return orient.w;
+        }
+        if(dir.y>0){
+            return orient.n;
+        }
+        return orient.s;
+    }
+}
