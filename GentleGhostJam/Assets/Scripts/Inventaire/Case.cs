4640a2844f6bb1ae6215b3401d5bcbcd513d1598
Leon <romain.delpy@ent.u-bordeaux.fr>
2019-10-24 05:05:08 +0200

    AJout halo inventaire

--- GentleGhostJam/Assets/Scripts/Inventaire/Case.cs
+++ GentleGhostJam/Assets/Scripts/Inventaire/Case.cs
@@ -1,9 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Case : MonoBehaviour
+public class Case : MonoBehaviour, IPointerDownHandler
 {
-    // Start is called before the first frame update
+    private Inventaire inventaire;
+    public GameObject mask;
+    public Color color;
+    private Color transparent;
     public int indice;
+    public bool clicked;
+
+
+    private void Start() {
+        transparent.a = 0;
+        inventaire = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventaire>();
+        mask.GetComponent<Image>().color = transparent;
+        clicked = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (!clicked && inventaire.listeObjets.lObj[indice].id!=-1){
+            mask.GetComponent<Image>().color = color;
+            clicked = true;
+        }
+        
+    }
 }
