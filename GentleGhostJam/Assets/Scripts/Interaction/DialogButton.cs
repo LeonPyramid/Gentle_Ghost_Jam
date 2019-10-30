3d31619e4c5a4194fe363e4a8e5a6f416ba40b03
Leon <romain.delpy@ent.u-bordeaux.fr>
2019-10-30 01:09:43 +0100

    Ajout des bases du niveau Tuto Faire gaffe dans le futur pour la portzbilité des scripts et autres

---
+++ GentleGhostJam/Assets/Scripts/Interaction/DialogButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DialogButton : MonoBehaviour , IPointerDownHandler
+{
+    public DialogManager dialog;
+    public void OnPointerDown(PointerEventData eventData){
+        dialog.Next();
+    }
+}
---
+++ GentleGhostJam/Assets/SpawnDialog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDialog : MonoBehaviour
+{
+    public List<DialogPage> dialogSpawn;
+    private DialogManager dialogManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogManager = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventaire>().dialogManager;
+        dialogManager.SetDialog(dialogSpawn);
+        GameObject.Destroy(this);
+    }
+
+}
